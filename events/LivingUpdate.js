27f63638827c79acdb0299fa51050e489892909d
1mi0 <smokemi0123@gmail.com>
2025-07-01 14:58:08 +0000

    fixed living update event

--- events/LivingUpdate.js
+++ events/LivingUpdate.js
@@ -1,6 +1,7 @@
 import { CancellableEvent } from "./CustomEvents"
 
-export default const LivingUpdate = new CancellableEvent();
+const LivingUpdate = new CancellableEvent();
+export default LivingUpdate;
 
 register(net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent, (event) => {
     if (event.entity !== Player.getPlayer()) return
