d8ef6e4cd41ff6c0013285cd94ff4a3d050d2168
1mi0 <smokemi0123@gmail.com>
2025-07-08 23:21:21 +0000

    fixed freeze manager I think

--- features/AutoRoutes/FreezeManager.js
+++ features/AutoRoutes/FreezeManager.js
@@ -1,18 +1,18 @@
 import OnUpdateWalkingPlayerPre from "../../events/OnUpdateWalkingPlayerPre"
 import Vector3 from "../../utils/Vector3"
-import { setVelocity, setPosition, removeCameraInterpolation } from "../../utils/utils"
+import { setVelocity, setPlayerPosition, removeCameraInterpolation } from "../../utils/utils"
 
 export default new class FreezeManager {
     constructor() {
         this.freezing = false
 
         this.position = new Vector3(Player);
-        this.previousVelocity = [Player.getMotionX(), Player.getMotionY(), Player.getMotionZ()]
+        this.previousVelocity = [Player.getMotionX(), Player.getMotionY(), Player.getMotionZ()];
         OnUpdateWalkingPlayerPre.register(event => {
             if (!this.freezing) return
 
             removeCameraInterpolation()
-            setPosition(this.position.x, this.position.y, this.position.z);
+            setPlayerPosition(this.position.x, this.position.y, this.position.z);
             setVelocity(0, 0, 0)
             event.cancelled = true
             event.break = true
@@ -21,7 +21,7 @@ export default new class FreezeManager {
 
     setFreezing(state) {
         if (state) {
-            this.previousVelocity = [Player.getMotionX(), Player.getMotionY(), Player.getMotionZ()]
+            this.previousVelocity = [Player.getMotionX(), Player.getMotionY(), Player.getMotionZ()];
             setVelocity(0, 0, 0)
             this.position = new Vector3(Player);
         }
