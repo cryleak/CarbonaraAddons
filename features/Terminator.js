5bc0f090ac5ca367d200c8276cb57d16cc159a43
cryleak <58886961+cryleak@users.noreply.github.com>
2025-07-15 20:48:03 +0200

    Refactor mixin system and update event handling
    Refactored mixin exports to use default exports and changed how mixins are registered and invoked. Updated event handlers for Terminator and PostPacketSend to use ChatLib for output. Adjusted 4thDevSimulator bow shooting logic. Improved descriptor parsing and fixed typos in Mixin.js. Changed PostPacketSendMixin injection point to ASM.At.TAIL.

--- features/Terminator.js
+++ features/Terminator.js
@@ -1,11 +1,10 @@
 import { registerCallback } from "../mixins/Callback"
 
 registerCallback("Terminator", (event, obj, itemStack, world, player) => {
+    ChatLib.chat("yo")
     return
-    console.log("Terminator right-click detected.");
     const wrappedItem = new Item(itemStack)
     if (wrappedItem.getLore().some(thing => thing.removeFormatting() === "Shortbow: Instantly shoots!")) {
-        console.log("Terminator right-click detected, cancelling action.");
         event.cancel();
         event.setReturnValue(itemStack);
     }
--- mixins/BowRightClickMixin.js
+++ mixins/BowRightClickMixin.js
@@ -1,6 +1,6 @@
 import Mixin from "./Mixin.js";
 
-Mixin(
+export default Mixin(
     "net.minecraft.item.ItemBow",
     "func_77659_a",
     "(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;",
