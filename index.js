cc2319f779acc60ab6357745b576138810778d65
1mi0 <smokemi0123@gmail.com>
2025-07-16 22:38:03 +0000

    post loader cleanup

--- index.js
+++ index.js
@@ -10,12 +10,3 @@ import "./features/BonzoSimulator"
 import "./features/Doors"
 import "./features/4thDevSimulator"
 import "./features/Terminator"
-import "./mixins/ASMFixer"
-
-import "./events/PostPacketSend"
-
-import { Callback, CancellableCallback, CancellableReturnableCallback } from "./mixins/Callback";
-
-export { CancellableReturnableCallback as mixinCancellableReturnableCallback };
-export { CancellableCallback as mixinCancellableCallback };
-export { Callback as mixinCallback };
--- mixins/BowRightClickMixin.js
+++
@@ -1,9 +0,0 @@
-import Mixin from "./Mixin.js";
-
-export default Mixin(
-    "net.minecraft.item.ItemBow",
-    "func_77659_a",
-    "(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;",
-    "Terminator",
-    ASM => ASM.At(ASM.At.HEAD)
-);
