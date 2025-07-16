454ca0ac24d3e735aca0bc43dfe1923f6a38ca28
1mi0 <smokemi0123@gmail.com>
2025-07-16 22:38:03 +0000

    post loader cleanup

--- mixins/PostPacketSendMixin.js
+++
@@ -1,9 +0,0 @@
-import Mixin from "./Mixin";
-
-export default Mixin(
-    "net/minecraft/network/NetworkManager",
-    "func_179290_a",
-    "(Lnet/minecraft/network/Packet;)V",
-    "PostPacketSend",
-    ASM => ASM.At(ASM.At.TAIL)
-)
