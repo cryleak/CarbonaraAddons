641c67052299f74db5897cf4d12719c41a98801c
1mi0 <smokemi0123@gmail.com>
2025-07-17 03:20:16 +0000

    added important documentation

--- events/JavaEvents.js
+++ events/JavaEvents.js
@@ -1,3 +1,4 @@
+import { Event, CancellableEvent } from "./CustomEvents";
 
 const events = Java.type("me.cryleak.carbonaraloader.event.Events");
 
@@ -7,4 +8,18 @@ register("gameUnload", () => {
 
 export const PostPacketSend = events.PostPacketSend;
 export const BowItemRightClick = events.BowItemRightClick;
+
+/**
+ * Represents data for an arrow that has landed on a block/tile.
+ * Contains the arrow entity and the coordinates of the tile it landed on.
+ * @class EntityArrowLandData
+ * @property {EntityArrow} arrow - The arrow entity that landed
+ * @property {number} xTile - The x-coordinate of the tile the arrow landed on
+ * @property {number} yTile - The y-coordinate of the tile the arrow landed on
+ * @property {number} zTile - The z-coordinate of the tile the arrow landed on
+ */
+
+/**
+ * @type {Event<EntityArrowLandData>}
+ */
 export const EntityArrowLand = events.EntityArrowLand
