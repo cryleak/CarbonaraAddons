c06d43b01200c068e3dbb8f5d7f69ccd4d477a49
1mi0 <smokemi0123@gmail.com>
2025-07-17 07:31:49 +0300

    Refactor type annotations for event handler and instance definitions

--- events/CustomEvents.js
+++ events/CustomEvents.js
@@ -6,8 +6,8 @@
 
 /**
  * @typedef {Object} EventHandler
- * @property {function(): EventHandler} register - Register an event listener.
- * @property {function(): EventHandler} unregister - Unregister the listener associated with this handler.
+ * @property {() => EventHandler} register - Register an event listener.
+ * @property {() => EventHandler} unregister - Unregister the listener associated with this handler.
  * @property {boolean} registered - Indicates if the listener is currently registered.
  * @description A handle to control the registration of a single listener. It allows you
  * to register or unregister the listener it's associated with.
@@ -16,10 +16,10 @@
 /**
  * @template T
  * @typedef {Object} EventInstance
- * @property {function(): boolean} hasListeners - Check if the event has any listeners
- * @property {function(number, EventListenerCallback<T>): EventHandler} scheduleTask - Schedule a task with delay
- * @property {function(EventListenerCallback<T>, number): EventHandler} register - Register an event listener with priority
- * @property {function(T): void} trigger - Trigger the event with data
+ * @property {() => boolean} hasListeners - Check if the event has any listeners
+ * @property {(delay: number, callback: EventListenerCallback<T>) => EventHandler} scheduleTask - Schedule a task with delay
+ * @property {(callback: EventListenerCallback<T>, priority: number = 1000) => EventHandler} register - Register an event listener with priority
+ * @property {(data: T) => void} trigger - Trigger the event with data
  */
 
 /**
