0fbe9c26f5dff95f30d84dee21c1438f9748ec8c
cryleak <58886961+cryleak@users.noreply.github.com>
2025-07-17 03:35:47 +0200

    add jsdoc types

--- events/JavaEvents.js
+++ events/JavaEvents.js
@@ -1,8 +1,12 @@
+import { CancellableEvent, Event } from "./CustomEvents";
+
 const events = Java.type("me.cryleak.carbonaraloader.event.Events");
 
 register("gameUnload", () => {
     events.unregisterAll();
 });
 
+/** @type {Event} */
 export const PostPacketSend = events.PostPacketSend;
+/** @type {CancellableEvent} */
 export const BowItemRightClick = events.BowItemRightClick;
