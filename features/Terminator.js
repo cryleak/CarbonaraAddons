1d0c0d77e107be5886bf8e524d333d354a8de613
1mi0 <smokemi0123@gmail.com>
2025-07-17 01:32:19 +0000

    made some stuff work

--- features/Terminator.js
+++ features/Terminator.js
@@ -1,7 +1,10 @@
-registerCallback("Terminator", (event, obj, itemStack, world, player) => {
+import { BowItemRightClick } from "../events/JavaEvents.js";
+
+BowItemRightClick.register((event) => {
+    const itemStack = event.data.itemStack;
     const wrappedItem = new Item(itemStack)
-    if (wrappedItem.getLore().some(thing => thing.removeFormatting() === "Shortbow: Instantly shoots!")) {
-        event.cancel();
-        event.setReturnValue(itemStack);
+    if (wrappedItem.getLore().some(l => l.removeFormatting() === "Shortbow: Instantly shoots!")) {
+        event.cancelled = true;
+        event.returnValue = itemStack;
     }
 });
