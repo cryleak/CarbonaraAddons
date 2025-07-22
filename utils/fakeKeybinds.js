4520675f039757592b17bb649bb97eccd5a502c1
cryleak <58886961+cryleak@users.noreply.github.com>
2025-07-22 20:41:38 +0200

    make fakekeybinds work with mouse

--- utils/fakeKeybinds.js
+++ utils/fakeKeybinds.js
@@ -3,21 +3,31 @@ import Settings from "../config"
 const fakeKeybinds = []
 
 function onKeyPress(keyBindName, exec) {
-    keyPressListener.register()
     fakeKeybinds.push({ keyBindName, exec })
 }
 
 export default { onKeyPress }
 
-const keyPressListener = register(net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent, () => {
+register(net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent, () => {
     if (Client.isInGui() || !World.isLoaded()) return
     if (!Keyboard.getEventKeyState()) return
     const keyCode = Keyboard.getEventKey()
     if (!keyCode) return
 
     const settings = Settings()
-    const pressedKeybinds = fakeKeybinds.filter(keybind => settings[keybind.keyBindName] == keyCode)
+    const pressedKeybinds = fakeKeybinds.filter(keybind => settings[keybind.keyBindName] === keyCode)
     if (!pressedKeybinds.length) return
 
     pressedKeybinds.forEach(keybind => keybind.exec()) // Execute the code associated with the pressed keybinds. There can be multiple on the same key.
-}).unregister()
+})
+
+register("clicked", (_, _, btn, state) => {
+    if (!World.isLoaded() || !state || Client.isInGui()) return
+
+    const button = btn - 100
+    const settings = Settings()
+    const pressedKeybinds = fakeKeybinds.filter(keybind => settings[keybind.keyBindName] === button)
+    if (!pressedKeybinds.length) return
+
+    pressedKeybinds.forEach(keybind => keybind.exec())
+})
